917dcc6d8c2deeea6ca5576811a55c40817af0d6
daspharion <daspharion@gmail.com>
2021-05-15 16:13:52 +0200

    header serves as a link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class='p-4 bg-gray-200 text-black'>
-    <div class='flex flex-row items-center'>
+    <router-link to='/' class='flex flex-row items-center'>
       <Bin class='w-8 h-8' />
       <h1 class='text-2xl ml-2'>bin</h1>
-    </div>
+    </router-link>
   </header>
 </template>
 
