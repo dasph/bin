9de5e3135b10b10e82b02edf061331521b92357b
daspharion <daspharion@gmail.com>
2021-06-12 22:08:48 +0200

    fixed header (widths is not 100%)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
-  <header class='p-4 bg-gray-200 text-black'>
+  <header class='p-4 bg-gray-200 text-black flex flex-row'>
     <router-link to='/' class='flex flex-row items-center'>
       <Bin class='w-8 h-8' />
       <h1 class='text-2xl ml-2'>bin</h1>
     </router-link>
+    <div class='flex' />
   </header>
 </template>
 
