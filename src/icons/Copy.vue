a788806d5ef3f9947756b8753fcb5af79b4f94de
daspharion <daspharion@gmail.com>
2021-06-14 03:28:42 +0200

    added new icons!

---
+++ src/icons/Copy.vue
@@ -0,0 +1,17 @@
+<template>
+  <Icon viewBox='0 0 48 48'>
+    <path d='m32,2l-24,0c-2.21,0 -4,1.79 -4,4l0,28l4,0l0,-28l24,0l0,-4zm6,8l-22,0c-2.21,0 -4,1.79 -4,4l0,28c0,2.21 1.79,4 4,4l22,0c2.21,0 4,-1.79 4,-4l0,-28c0,-2.21 -1.79,-4 -4,-4zm0,32l-22,0l0,-28l22,0l0,28z' />
+  </Icon>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from './Icon.vue'
+
+export default defineComponent({
+  name: 'IconCopy',
+  components: { Icon }
+})
+</script>
+
+
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as Bin } from './Bin.vue'
 export { default as Error } from './Error.vue'
+export { default as Copy } from './Copy.vue'
+export { default as Lock } from './Lock.vue'
